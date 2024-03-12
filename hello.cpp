495334d41507d377da46c38d5ba55e9ee080abea
Park Gyutae <162317836+2001pgt@users.noreply.github.com>
2024-03-12 19:53:27 +0900

    cpp2-1 실습 학번,학과,이름 출력하기

--- hello.cpp
+++ hello.cpp
@@ -1,26 +1,20 @@
-#include<iostream>
-
-using namespace std;//std이름 공간에 선언된 모든 이름에 std::생략
+#include <iostream>
+#include<string>
+using namespace std;
 
 int main() {
-	cout << "학번을 입력하세요";
+	cout << "학번,학과,이름을 입력하세요";
 
-	int 학번;//변수 "학번"은 정수이므로 int사용
-	cin >> 학번;//학번 입력 
+	int 학번;
+	string 학과;
+	string 이름;
 
-	cout << "학과를 입력하세요";
-	string 학과;//변수 "학과"는 문자열이므로string사용
-	cin >> 학과;//학과 입력
+	cin >> 학번;
+	cin >> 학과;
+	cin >> 이름;
 
-	cout << "이름을 입력하세요";
-	string 이름;//변수 "이름"은 문자열이므로string사용
-	cin >> 이름;//이름 입력
-
-	cout << "학번:" << 학번;//학번:_,학과:_,이름:_형태로 출력
-	cout << ",학과:" << 학과;
-	cout << ",이름:" << 이름;
-
-	
-
+	cout << "학번" << 학번 << "학과" << 학과 << "이름" << 이름;
 
 }
+
+	
