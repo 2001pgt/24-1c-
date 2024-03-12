a72efc08411deb19f0485eaad51912c84fc99659
Park Gyutae <162317836+2001pgt@users.noreply.github.com>
2024-03-12 19:45:49 +0900

    Add files via upload

---
+++ hello.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include<string>
+using namespace std;
+
+int main() {
+	cout << "학번,학과,이름을 입력하세요";
+
+	int 학번;
+	string 학과;
+	string 이름;
+
+	cin >> 학번;
+	cin >> 학과;
+	cin >> 이름;
+
+	cout << "학번" << 학번 << "학과" << 학과 << "이름" << 이름;
+
+}
+
+	
